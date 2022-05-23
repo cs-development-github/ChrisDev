cf6013dcb5d14b2af00ebef6c01d27a28eb3117f
christopher vermersch <cs.communication.dev@gmail.com>
2022-05-23 19:28:25 +0200

    first commit

---
+++ js/mail.js
@@ -0,0 +1,12 @@
+function sendMail(){
+    var params = {
+        form_name : document.getElementById("form_name").value,
+        form_email: document.getElementById("form_email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value
+    }
+
+    emailjs.send("service_v6kki0k", "template_wuc0jlr", params).then( function(res){
+        alert("success" + res.status);
+    })
+}
