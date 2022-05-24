3954d2e3b2327c4248c38eaf6092f0f945c42568
christopher vermersch <cs.communication.dev@gmail.com>
2022-05-24 15:33:52 +0200

    add icon

--- js/mail.js
+++ js/mail.js
@@ -3,7 +3,8 @@ function sendMail(){
         form_name : document.getElementById("form_name").value,
         form_email: document.getElementById("form_email").value,
         subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value
+        message: document.getElementById("message").value,
+        category: document.getElementById("category_form").value
     }
 
     emailjs.send("service_v6kki0k", "template_wuc0jlr", params).then( function(res){
